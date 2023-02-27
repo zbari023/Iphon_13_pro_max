782322368922313ab54ec7393d8e3bd6883b7525
zbari023 <barry450643@gmail.com>
2023-02-27 20:30:57 +0100

    final product

---
+++ code.js
@@ -0,0 +1,10 @@
+let imgjs = document.querySelector('.imgjs');
+let backjs= document.querySelector('.allcontainer');
+
+function Iphons(Iphonsart){
+imgjs.src = Iphonsart;
+
+}
+function backgroundcharging(colorchanging){
+    backjs.style.background = colorchanging;
+}
